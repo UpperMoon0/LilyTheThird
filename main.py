6b29ec2a4e1c6eb3b86423078bc58127c1f6cd1a
UpperMoon0 <60876500+UpperMoon0@users.noreply.github.com>
2025-04-21 18:09:22 +0700

    Refactor tab management in MainAppLayout: Update TabbedPanel to set default tab after adding tabs, enhancing tab initialization logic and ensuring correct default selection.

--- main.py
+++ main.py
@@ -21,27 +21,28 @@ class MainAppLayout(BoxLayout):
         # Orientation is vertical by default for BoxLayout
 
         # Create the TabbedPanel
-        tab_panel = TabbedPanel(do_default_tab=False, tab_pos='top_mid') # No default tab, tabs on top
+        # Set do_default_tab=True and ensure default_tab is set after adding the desired tab.
+        tab_panel = TabbedPanel(do_default_tab=True, tab_pos='top_mid', tab_width=150) # Default tab enabled, tabs on top
 
-        # --- Create and add VTube Tab ---
-        vtube_tab_item = TabbedPanelItem(text='VTube')
-        vtube_tab_content = VTubeTab() # Instantiate the VTubeTab widget
-        vtube_tab_item.add_widget(vtube_tab_content)
-        tab_panel.add_widget(vtube_tab_item)
-
-        # --- Create and add Chat Tab ---
+        # --- Create and add Chat Tab (First Tab) ---
         chat_tab_item = TabbedPanelItem(text='Chat')
         chat_tab_content = ChatTab() # Instantiate the ChatTab widget
         chat_tab_item.add_widget(chat_tab_content)
         tab_panel.add_widget(chat_tab_item)
         # Set Chat Tab as the default selected tab
-        tab_panel.default_tab = chat_tab_item
+        tab_panel.default_tab = chat_tab_item # Set this AFTER adding the chat tab
 
-        # --- Create and add Discord Tab ---
+        # --- Create and add Discord Tab (Second Tab) ---
         discord_tab_item = TabbedPanelItem(text='Discord')
         discord_tab_content = DiscordTab() # Instantiate the DiscordTab widget
         discord_tab_item.add_widget(discord_tab_content)
         tab_panel.add_widget(discord_tab_item)
+
+        # --- Create and add VTube Tab (Third Tab) ---
+        vtube_tab_item = TabbedPanelItem(text='VTube')
+        vtube_tab_content = VTubeTab() # Instantiate the VTubeTab widget
+        vtube_tab_item.add_widget(vtube_tab_content)
+        tab_panel.add_widget(vtube_tab_item)
 
         # Add the TabbedPanel to the root layout
         self.add_widget(tab_panel)
